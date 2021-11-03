ec1dfd0bb22a223928da4a195b466cca6f139eb1
djy-git <djyoon0223@gmail.com>
2021-11-04 00:33:52 +0900

    initial commit

---
+++ playground/main.py
@@ -0,0 +1,9 @@
+### main.py ###################################
+# Example codes are in here
+###############################################
+
+
+from util import *
+
+if __name__ == '__main__':
+    print('Hello, world!')
